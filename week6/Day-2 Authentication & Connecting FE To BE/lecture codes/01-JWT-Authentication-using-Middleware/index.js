9175512e649c3c55808d3081e1a0d5e3db1bccdd
nodeanurag <conveytoanurag@gmail.com>
2026-06-08 20:17:14 +0530

    wrote signin endpoint in index.js

--- week6/Day-2 Authentication & Connecting FE To BE/lecture codes/01-JWT-Authentication-using-Middleware/index.js
+++ week6/Day-2 Authentication & Connecting FE To BE/lecture codes/01-JWT-Authentication-using-Middleware/index.js
@@ -29,3 +29,31 @@ app.post("/signup",logger, function(req, res){
         meassage:"Signed in successfully!"
     });
 })
+app.post("/signin",logger, function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser =null;
+
+    for (let i = 0; i < users.length; i++){
+        if(users[i].username === username && users[i].password === password){
+            foundUser = users[i]
+        }        
+    }
+
+    if(!foundUser){
+        res.json({
+            message:"Invalid Credentials"
+        })
+    }
+    else{
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+    }
+
+})
